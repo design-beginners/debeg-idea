229dad374898dc69eabcc0f9a971eccad4963696
willnet <netwillnet@gmail.com>
2014-04-23 19:35:15 +0900

    Add omniauth secret key and settings

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :twitter,
+    Rails.application.secrets.twitter_consumer_key,
+    Rails.application.secrets.twitter_consumer_secret
+end
+
+OmniAuth.config.on_failure = Proc.new { |env|
+  OmniAuth::FailureEndpoint.new(env).redirect_to_failure
+}
