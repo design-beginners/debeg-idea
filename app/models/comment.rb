5281dad5ad322761df029e5bbba1908b4ec07d69
willnet <netwillnet@gmail.com>
2014-04-28 23:01:48 +0900

    Add user column to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :idea
+  belongs_to :user
 
   validates :body, length: { maximum: 1000 }, presence: true
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -4,4 +4,5 @@ describe Comment do
   it { should belong_to(:idea) }
   it { should validate_presence_of(:body) }
   it { should ensure_length_of(:body).is_at_most(1000) }
+  it { should belong_to(:user) }
 end
