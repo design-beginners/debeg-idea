58b01614d0572c417073e46feca22a1af2c94c16
willnet <netwillnet@gmail.com>
2014-05-01 01:04:24 +0900

    Add factory_girl setting for comment

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
   factory :comment do
+    idea
+    user
+    sequence(:body) { |i| "コメント" }
   end
 end
