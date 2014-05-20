ad1741714fd758135657eafc8931a950eeacb5a4
willnet <netwillnet@gmail.com>
2014-05-20 15:16:31 +0900

    Delete unnecessary factory file

--- spec/factories/favorites.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :favorite do
-    user_id 1
-    target_id 1
-    target_type "MyString"
-  end
-end
