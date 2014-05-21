96b525e74c5ebfe2ba8342578a46e5c026195a03
willnet <netwillnet@gmail.com>
2014-05-21 18:47:33 +0900

    Enable no-login users to add ideas

--- spec/features/create_idea_spec.rb
+++ spec/features/create_idea_spec.rb
@@ -5,8 +5,23 @@ describe 'やってみたいことをアイデアとして投稿する' do
   context '未ログイン状態でトップページにアクセスしたとき' do
     before { visit root_path }
 
-    it 'アイデア投稿用のフォームが表示されていないこと' do
-      expect(page).to have_no_css '#new_idea'
+    it 'アイデア投稿用のフォームが表示されていること' do
+      expect(page).to have_css '#new_idea'
+    end
+
+    context 'かつ、アイデアを入力して"アイデアを送信する"をクリックしたとき' do
+      before do
+        fill_in 'idea_title', with: '素晴らしいアイデア'
+        click_button 'アイデアを送信する'
+      end
+
+      it '"アイデアを追加しました！"と表示されていること' do
+        expect(page).to have_content 'アイデアを追加しました！'
+      end
+
+      it '登録したアイデアが表示されていること' do
+        expect(page).to have_content '素晴らしいアイデア'
+      end
     end
   end
 
