37051e5bdf598edb69f8c6c6cb92f8c781babb90
willnet <netwillnet@gmail.com>
2014-07-23 11:23:07 +0900

    Delete extra line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, alert: 'ログインしてください'
   end
 
-
   def error404
     render 'error404', status: 404, formats: [:html]
   end
