4dc4723a8be668b39712249891e79ea2f2cbace6
willnet <netwillnet@gmail.com>
2014-07-29 21:38:24 +0900

    Enable to notify to idobata when create idea or comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,8 +7,19 @@ class Comment < ActiveRecord::Base
 
   validates :body, length: { maximum: 1000 }, presence: true
 
+  after_save :notify_to_idobata, if: Proc.new { Rails.env.production? }
+
   def created_by?(user)
     return false unless user
     user_id == user.id
+  end
+
+  def notify_to_idobata
+    idobata_uri = URI.parse('https://idobata.io/hook/generic/ff5734c7-fb93-4085-87ae-ab58af06cb22')
+    message = "@#{user.nickname} created comment to #{idea.title}: #{body}"
+
+    Net::HTTP.post_form(idobata_uri, source: message)
+  rescue => e
+    logger.error [e, *e.backtrace].join("\n")
   end
 end
--- app/models/idea.rb
+++ app/models/idea.rb
@@ -7,8 +7,22 @@ class Idea < ActiveRecord::Base
 
   validates :title, length: { maximum: 100 }, presence: true
 
+  after_save :notify_to_idobata, if: Proc.new { Rails.env.production? }
+
   def created_by?(user)
     return false unless user
     user_id == user.id
+  end
+
+  def notify_to_idobata
+    idobata_uri = URI.parse('https://idobata.io/hook/generic/ff5734c7-fb93-4085-87ae-ab58af06cb22')
+    message = if user
+                "@#{user.nickname} created idea!: #{title}"
+              else
+                "Someone created idea!: #{title}"
+              end
+    Net::HTTP.post_form(idobata_uri, source: message)
+  rescue => e
+    logger.error [e, *e.backtrace].join("\n")
   end
 end
